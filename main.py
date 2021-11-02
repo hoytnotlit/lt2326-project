using wrong dimension in softmax

main.py
@@ -15,7 +15,8 @@ if __name__ == "__main__":
 
     device = torch.device(args.device)
     retrain = args.retrain
-    n_quotes = args.n_quotes
+    n_quotes = int(args.n_quotes)
+
     # get data for training, vocab lenght for model parameter 
     # and word-int-word maps & all tokenized sentences for text generation
     input_data, vocab_len, word_to_int, int_to_word, tokenized = data.get()

test.py
@@ -13,7 +13,7 @@ def generate_quote(model, seed, int_to_word, device):
         model_input = torch.tensor(seed).unsqueeze(0).to(device)
         with torch.no_grad():
             output = model(model_input)
-        output = F.softmax(output, dim=1) # get word probabilities
+        output = F.softmax(output, dim=2) # get word probabilities
         output = output.squeeze(0)[-1] # we only need the last word
         top_predictions = output.argsort()[-5:] # select top 5 values
         selected_prediction = int(random.choice(top_predictions)) # select one of the top predictions

train.py
@@ -27,7 +27,7 @@ def train(input_data, vocab_len, device):
             inputs, targets = inputs.to(device), targets.to(device) # send tensors to device
             outputs = lstm(inputs)
 
-            outputs = F.softmax(outputs, dim=1) # TODO why in tutorials softmax not there?
+            outputs = F.softmax(outputs, dim=2)
             loss = loss_fn(outputs.permute(0, 2, 1), targets) # calculate loss
             total_loss += loss.item()
             print(f'{total_loss/(i+1)} batch n.{batch_n}', end='\r') # print average loss for the epoch